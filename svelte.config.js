00229e2974f344260f43c93b54d53d1900e82576
Rafi Strauss <rafikis75@gmail.com>
2021-05-12 19:47:32 -0400

    Add buiild steps

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		})
 	}
 };
-
-export default config;
